ce0fd71a5adfed222ccccf3b88f1bc9139f33a71
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-24 15:53:51 +0200

    add default user

--- django/cvApp/views.py
+++ django/cvApp/views.py
@@ -1,21 +1,21 @@
-from rest_framework import serializers, generics, permissions
+from rest_framework import serializers, generics
 from .models import Cv
 
 class CvSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cv
-        fields = '__all__'
+	user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+	class Meta:
+		model = Cv
+		fields = '__all__'
 
 class CvCreateView(generics.CreateAPIView):
-    serializer_class = CvSerializer
-    permission_classes = [permissions.IsAuthenticated]
+	serializer_class = CvSerializer
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+	def perform_create(self, serializer):
+		serializer.save(user=self.request.user)
 
 class CvDetailView(generics.RetrieveAPIView):
-    serializer_class = CvSerializer
-    permission_classes = [permissions.IsAuthenticated]
+	serializer_class = CvSerializer
 
-    def get_object(self):
-        return Cv.objects.get(user=self.request.user)
+	def get_object(self):
+		return Cv.objects.get(user=self.request.user)
