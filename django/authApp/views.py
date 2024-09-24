c69c7b2a9a617091cae367370afed867ef691b91
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-24 15:36:31 +0200

    indent tabs

--- django/authApp/views.py
+++ django/authApp/views.py
@@ -3,21 +3,21 @@ from .models import CustomUser
 from rest_framework.permissions import AllowAny
 
 class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ('username', 'password', 'email', 'current_cv')
-        extra_kwargs = {'password': {'write_only': True}}
+	class Meta:
+		model = CustomUser
+		fields = ('username', 'password', 'email', 'current_cv')
+		extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):
-        user = CustomUser.objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['password'],
-            email=validated_data['email'],
-            current_cv=validated_data['current_cv']
-        )
-        return user
+	def create(self, validated_data):
+		user = CustomUser.objects.create_user(
+			username=validated_data['username'],
+			password=validated_data['password'],
+			email=validated_data['email'],
+			current_cv=validated_data['current_cv']
+		)
+		return user
 
 class RegisterView(generics.CreateAPIView):
-    queryset = CustomUser.objects.all()
-    permission_classes = AllowAny
-    serializer_class = UserSerializer
+	queryset = CustomUser.objects.all()
+	permission_classes = AllowAny
+	serializer_class = UserSerializer
