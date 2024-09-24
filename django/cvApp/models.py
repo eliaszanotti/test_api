a00a48565c16da3ea59a96ac3a1c11f2c8164e3b
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-24 15:41:35 +0200

    add cvApp

--- django/cvApp/models.py
+++ django/cvApp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from authApp.models import CustomUser
 
-# Create your models here.
+class Cv(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='cvs')
+    name = models.CharField(max_length=50, blank=True, default="Nouveau CV")
--- django/mainProject/settings.py
+++ django/mainProject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'authApp',
+    'cvApp',
 	'personnalApp'
 ]
 
