236529e0f3c3b51682692aaf7374e4fc1c953776
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-24 17:00:07 +0200

    add validation for personnal data

--- django/personnalApp/models.py
+++ django/personnalApp/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from cvApp.models import Cv
+from django.core.exceptions import ValidationError
+
+def validate_age(value):
+	if value < 0 or value > 150:
+		raise ValidationError("Age must be between 0 and 150")
+
+def validate_postal_code(value):
+	if not value.isdigit() or len(value) != 5:
+		raise ValidationError("Postal code must be a 5-digit number")
 
 class Personnal(models.Model):
 	cv = models.OneToOneField(Cv, on_delete=models.CASCADE, related_name='personnal')
@@ -8,14 +17,14 @@ class Personnal(models.Model):
 	name = models.CharField(max_length=50, blank=True)
 	first_name = models.CharField(max_length=50, blank=True)
 	phone = models.CharField(max_length=20, blank=True)
-	# email = EmailField(max_length=254, blank=True)
-	# age = AgeField(null=True)
-	# birthdate = DateField(max_length=10, blank=True)
+	email = models.EmailField(max_length=254, blank=True)
+	age = models.IntegerField(validators=[validate_age], null=True, blank=True)  # Custom age field with validation
+	birthdate = models.DateField(null=True, blank=True)
 	additional = models.CharField(max_length=50, blank=True)
-	# postal_code = PostalCodeField(null=True)
+	postal_code = models.CharField(validators=[validate_postal_code], max_length=10, blank=True)
 	city = models.CharField(max_length=50, blank=True)
 	country = models.CharField(max_length=50, blank=True)
-	# license = models.CharField(max_length=50, default=LICENSE_CHOICES[0])
+	license = models.CharField(max_length=50, blank=True)
 	other_license = models.CharField(max_length=50, blank=True)
 	has_vehicle = models.BooleanField(default=False)
 	range = models.CharField(max_length=100, blank=True)
