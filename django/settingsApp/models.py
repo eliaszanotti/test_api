02fca6fe49de8ae596cdd3d6a1e9fbb7a43aae11
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-26 12:13:00 +0200

    add default when creating settings object

--- django/settingsApp/models.py
+++ django/settingsApp/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from cvApp.models import Cv
 from commonApp.constants import SETTINGS_FONT_SIZES_CHOICES
+from .schemes import TEMPLATES
 
 class Settings(models.Model):
 	cv = models.OneToOneField(Cv, on_delete=models.CASCADE, related_name='settings')
 	template = models.IntegerField(default=0)
 	experience_first = models.BooleanField(default=True)
-	primary_color = models.CharField(max_length=7, blank=True)
-	secondary_color = models.CharField(max_length=7, blank=True)
-	third_color = models.CharField(max_length=7, blank=True)
-	dark_color = models.CharField(max_length=7, blank=True)
-	light_color = models.CharField(max_length=7, blank=True)
+	primary_color = models.CharField(max_length=7, blank=True, default=TEMPLATES[0]['primary'][0])
+	secondary_color = models.CharField(max_length=7, blank=True, default=TEMPLATES[0]['secondary'][0])
+	third_color = models.CharField(max_length=7, blank=True, default=TEMPLATES[0]['third'][0])
+	dark_color = models.CharField(max_length=7, blank=True, default=TEMPLATES[0]['dark'][0])
+	light_color = models.CharField(max_length=7, blank=True, default=TEMPLATES[0]['light'][0])
 	size = models.CharField(max_length=50, blank=True, default=SETTINGS_FONT_SIZES_CHOICES[0])
